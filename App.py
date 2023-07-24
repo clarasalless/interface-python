67f9e88c714c7d38de96170b4e7bcffb9f5276b3
dan-alencar <daniloalencar@alu.ufc.br>
2023-07-23 22:17:12 -0300

    Resolvido

--- App.py
+++ App.py
@@ -1,9 +1,18 @@
 import customtkinter as ctk
 import tkinter as tk
+from tkinter import filedialog
 from tkinter import messagebox
 from myframe import MyFrame
 from InitFrame import InitFrame
 
+def onOpen():
+    print(filedialog.askopenfilename(initialdir = "/",title = "Abrir arquivo",filetypes = (("Arquivos .lesc","*.lesc"),("Todos os arquivos","*.*"))))
+ 
+def onSave():
+    data = [('Arquivos .lesc', '*.lesc')]
+    file = filedialog.asksaveasfilename(initialdir = "/",title = "Salvar como",filetypes = data, defaultextension = data)
+    with open(file, "w") as f:
+        f.write("teste")
 
 def gerarbinario():
     for frame in MyFrame.all:
@@ -22,23 +31,18 @@ class App(ctk.CTk):
         self.geometry("800x600")
         self.title("Essa é a minha janela")
         self.iconbitmap("weg-logo-5.ico")
-
-        init_label = ctk.CTkLabel(
-            self, text="Files(Download)", padx=5, pady=5)
-        init_label.pack(anchor=ctk.NW)
         self.init_frame = InitFrame(
             master=self, corner_radius=0, fg_color="transparent")
         self.init_frame.pack(fill=ctk.BOTH, expand=ctk.TRUE)
         self.generate_binary = ctk.CTkButton(
             self, text="Gerar Binário", command=gerarbinario)
         self.generate_binary.pack(pady=10, padx=10)
-        menubar = tk.Menu(self)
-        filemenu = tk.Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Open")
-        filemenu.add_command(label="Save")
-
-        menubar.add_cascade(label="File", menu=filemenu)
-        self.config(menu=menubar)
+        self.menubar = tk.Menu(self)
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Abrir arquivo", command=onOpen)
+        self.filemenu.add_command(label="Salvar", command=onSave)
+        self.menubar.add_cascade(label="Arquivo", menu=self.filemenu)
+        self.config(menu=self.menubar)
 
 
 
